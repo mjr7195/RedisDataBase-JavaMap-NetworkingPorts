cc750e2175e505c0b670423b8a80b5e9bad9ec01
mjr7195 <mjr7195@psu.edu>
2023-11-07 16:21:47 -0500

    got CSV ports into a treemap.

--- CSVReader.java
+++ CSVReader.java
@@ -8,27 +8,30 @@ public class CSVReader {
     public static void main(String[] args) {
         String pathToCsv = "service-names-port-numbers.csv"; // replace with your CSV file path
         String line;
+        Map<String, String> portDescriptions = new TreeMap<>(); //Sorted
 
         try (BufferedReader br = new BufferedReader(new FileReader(pathToCsv))) {
 
             while ((line = br.readLine()) != null) {
                 // Use comma as separator
                 String[] columns = line.split(",");
-
                 // Print the content on the console
                 // for (String column : columns) {
                 try {
-                    System.out.print(columns[1] + " " + columns[3]);
+                    portDescriptions.put(columns[1], columns[3]);
                 }catch (Exception e){
                     // dont care
                 }
                 //}
-                System.out.println();
             }
-
 
         } catch (IOException e) {
             e.printStackTrace();
+        } catch (NumberFormatException e) {
+            // do nothing
         }
+
+        portDescriptions.forEach((key, value) -> System.out.println(key+ " " + value));
+        System.out.println("program was able to finish");
     }
 }
